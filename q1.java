0b818683d24868bf45af282cc9f292da520061bb
NickprimeBR <nickprime2112@gmail.com>
2025-04-11 20:00:06 -0300

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("digite a base do triangulo:");
+        Integer.valueOf base = entrada.nextLine();
+        
+    }
+}
