5bb2cc328f26be6b2d27cec8ee35c2f19fd2e065
NickprimeBR <nickprime2112@gmail.com>
2025-04-12 13:32:12 -0300

    q1.java

--- q1.java
+++ q1.java
@@ -3,7 +3,10 @@ class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.print("digite a base do triangulo:");
-        Integer.valueOf base = entrada.nextLine();
-        
+        int base = Integer.valueOf(entrada.nextLine());
+        System.out.print("digite a altura do triangulo:");
+        int altura = Integer.valueOf(entrada.nextLine());
+        double area = (base * altura)/2;
+        System.out.print(area);
     }
 }
